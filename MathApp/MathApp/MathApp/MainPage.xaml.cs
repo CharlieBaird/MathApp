0e807e4d631c3e3d04ace7a762a280cf5770d33c
Charlie Baird <charliembaird@gmail.com>
2020-09-05 21:23:42 -0700

    Add CSharpMath library

--- MathApp/MathApp/MathApp/MainPage.xaml.cs
+++ MathApp/MathApp/MathApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharpMath.Forms;
 using Xamarin.Forms;
 
 namespace MathApp
@@ -12,6 +13,17 @@ namespace MathApp
         public MainPage()
         {
             InitializeComponent();
+
+            for (var i = 0; i < 20; i++)
+            {
+                StackLayout.Children.Add(new MathView
+                {
+                    LaTeX = @"a^p=1",
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    BackgroundColor = Color.Beige
+                });
+            }
         }
     }
 }
